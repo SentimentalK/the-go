d2d93213ec02d56ccc879647e9460f3b626b18ac
Kevin Xu <kevinxu.senti@gmail.com>
2026-05-19 22:45:06 -0400

    project 1

---
+++ project01/main.go
@@ -0,0 +1,15 @@
+/*
+Author: Xinghan Xu
+Course: CST8002 Programming Language Research Project
+Professor: Stanley Pieda
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("test")
+}
