cf4710961cd38ee849e723796a28559ef1d89a2a
Kevin Xu <kevinxu.senti@gmail.com>
2026-06-01 14:47:13 -0400

    fix IEEE

--- project01/main.go
+++ project01/main.go
@@ -4,9 +4,14 @@ Course: CST8002 Programming Language Research Project
 Professor: Stanley Pieda
 
 References:
-[1] The Go Authors, "Package csv," pkg.go.dev. [Online]. Available: https://pkg.go.dev/encoding/csv. [Accessed: May 2026].
-[2] The Go Authors, "Package os," pkg.go.dev. [Online]. Available: https://pkg.go.dev/os. [Accessed: May 2026].
-[3] Fisheries and Oceans Canada, "Spatiotemporal variation in anadromous Arctic char..." open.canada.ca. [Online]. Available: https://open.canada.ca/data/en/dataset/9cbcf710-a2a1-11ef-8ccf-55cc7f028297. [Accessed: Apr. 30, 2026].
+[1] The Go Authors. (n.d.). Package csv. pkg.go.dev.
+    [online]. Available at https://pkg.go.dev/encoding/csv [Accessed on: May 2026].
+
+[2] The Go Authors. (n.d.). Package os. pkg.go.dev.
+    [online]. Available at https://pkg.go.dev/os [Accessed on: May 2026].
+
+[3] Fisheries and Oceans Canada. (2024, Dec. 16). Spatiotemporal variation in anadromous Arctic char (Salvelinus alpinus) foraging ecology and its influence on muscle pigmentation along western Hudson Bay, Nunavut, Canada. open.canada.ca.
+    [online]. Available at https://open.canada.ca/data/en/dataset/9cbcf710-a2a1-11ef-8ccf-55cc7f028297 [Accessed on: Apr. 30, 2026].
 */
 
 package main
